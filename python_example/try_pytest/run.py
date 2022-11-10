e3384915e99ac88019ae76d66b2596cb1d2fae34
zenghui <784515773@qq.com>
2022-11-10 22:59:50 +0800

    try pytest

---
+++ python_example/try_pytest/run.py
@@ -0,0 +1,9 @@
+import os
+import pytest
+
+
+
+
+
+if __name__ == "__main__":
+    pytest.main(["--html=log/report.html"])
---
+++ python_example/try_pytest/test_math.py
@@ -0,0 +1,7 @@
+import os
+
+def plus(a, b):
+    return a + b
+
+def test_plus():
+    assert plus(1, 2) == 3, "ERRORED"
