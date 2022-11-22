029a52e5866b3b049a8929b83fff4a9864d4ca51
zenghui <784515773@qq.com>
2022-11-22 21:31:29 +0800

    add some pytest example about search testcases

--- python_example/try_pytest/test_math.py
+++ python_example/try_pytest/test_math.py
@@ -3,5 +3,39 @@ import os
 def plus(a, b):
     return a + b
 
-def test_plus():
+def minus(a, b):
+    return a - b
+
+
+def testplus():
     assert plus(1, 2) == 3, "ERRORED"
+
+
+
+class TestMinus(object):
+
+    def minus_test():
+        assert minus(3, 1) == 2, "ERRORED"
+
+class Test_Minus(object):
+
+    def test_minus():
+        assert minus(3, 1) == 2, "ERRORED"
+
+
+
+class TestPlus(object):
+
+    def __init__(self):
+        pass
+
+    def test_plus(self):
+        assert plus(1, 2) == 3, "ERRORED"
+
+
+
+class Math(object):
+
+    def test_plus():
+        assert plus(1, 2) == 3, "ERRORED"
+
