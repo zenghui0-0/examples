b529ccfc78e22e184f200c6bc7d0d178a0ae6efe
zenghui <784515773@qq.com>
2022-11-25 01:29:38 +0800

    try multi thread in pytest

---
+++ python_example/try_pytest/test_multi_thread.py
@@ -0,0 +1,74 @@
+import os
+import time
+import pytest
+import random
+import threading
+
+def three_d_mark(run_time):
+    print("[os_tests] 3dmark running...")
+    time.sleep(run_time)
+    print("[os_tests] 3dmark finished.")
+    return random.randint(0,1)
+
+
+def memory_leak(run_time):
+    print("[os_tests] memory_leak running...")
+    time.sleep(run_time)
+    print("[os_tests] memory_leak finished.")
+    return random.randint(0,1)
+
+
+
+class MyThread(threading.Thread):
+
+    def __init__(self, func, args=()):
+        super().__init__()
+        self.func = func
+        self.args = args
+        self.result = 0
+
+    def run(self):
+        self.result = self.func(*self.args)
+
+    def join(self):
+        threading.Thread.join(self)
+        try:
+            return self.result
+        except Exception:
+            return None 
+
+@pytest.mark.parametrize('td_time, ml_time', [(30, 20)])
+def test_threads_join(td_time, ml_time):
+    """
+    3dmark memoryleak should both end normally, 
+    before test case end
+    """
+    t_3dmark = MyThread(three_d_mark, [td_time])
+    t_3dmark.start()
+    t_memoryleak = MyThread(memory_leak, [ml_time])
+    t_memoryleak.start()
+    ret1 = t_3dmark.join()
+    ret2 = t_memoryleak.join()
+    assert False, f'Failed, 3dmark return: {ret1}, memoryleak return: {ret2}'
+    print("Test 3dmark, memoryleak PASSED.")
+
+
+def test_threads_faild_beak():
+    """
+    3dmark or memoryleak should both end 
+    when one of them got failure
+    then test case end
+    """
+    pass
+
+
+def test_threads_force_beak():
+    """
+    fore all test thread end by a signal
+    then test case end
+    """
+    pass
+
+
+if __name__ == "__main__":
+    test_threads_join(30, 20)
--- python_example/try_threads.py
+++ python_example/try_threads.py
@@ -1,15 +1,19 @@
 import time
 import threading
 
-def test(a):
-    while True:
-        print("sleep {} test".format(a))
-        time.sleep(a)
+def test(gap_time, total_time):
+    start_time = 0
+    while start_time < total_time:
+        print("sleep {} test running, {} past...".format(gap_time, start_time))
+        start_time += gap_time
+        time.sleep(1)
+        #time.sleep(gap_time)
+    print("sleep {} test end.".format(gap_time))
 
 
-test1 = threading.Thread(target=test, args=((1,)))
+test1 = threading.Thread(target=test, args=((1, 20)))
 test1.start()
 
-test2 = threading.Thread(target=test, args=((2,)))
+test2 = threading.Thread(target=test, args=((2, 20)))
 test2.start()
-test2.join()
+#test2.join()
