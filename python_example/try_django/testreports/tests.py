8cc7c72b75d512a9dd159f9772822c90b243392c
zenghui <784515773@qq.com>
2023-05-10 16:55:37 +0800

    Update tests.py

--- python_example/try_django/testreports/tests.py
+++ python_example/try_django/testreports/tests.py
@@ -30,8 +30,8 @@ def catch_exception(func):
 @catch_exception
 def get_testreport(*args):
     report_id = int(args[0])
-    apis = "{0}/testreport/?id={1}".format(host_name, report_id)
-    res_get = requests.get(url=apis, headers=head, timeout=5)
+    apis = "{0}/testreports/?id={1}".format(host_name, report_id)
+    res_get = requests.get(url=apis, headers=head, proxies=proxies, timeout=5)
     if res_get.status_code == 200:
         print(res_get.text)
     else:
@@ -40,12 +40,12 @@ def get_testreport(*args):
 
 @catch_exception
 def create_testreport(*args):
-    apis = "{0}/testreport/".format(host_name)
+    apis = "{0}/testreports/".format(host_name)
     project_name, project_type, jenkins_url, artifactory_url, requester = \
         args[0], args[1], args[2], args[3], args[4]
     post_data = {"project_name": project_name, "jenkins_url": jenkins_url, "requester_ip": requester_ip,
                  "artifactory_url": artifactory_url, "type": TEST_REPORT_TYPE[project_type], "requester": requester}
-    res_post = requests.post(url=apis, headers=head,
+    res_post = requests.post(url=apis, headers=head, proxies=proxies,
                         data=json.dumps(post_data), timeout=10)
     if res_post.status_code == 201:
         print("report_id={0}".format(json.loads(res_post.text)["data"]["id"]))
@@ -58,10 +58,10 @@ def create_testreport(*args):
 @catch_exception
 def update_testreport_step(*args):
     report_id, run_status, run_step = args[0], args[1], args[2]
-    apis = "{0}/testreport/{1}".format(host_name, report_id)
+    apis = "{0}/testreports/{1}".format(host_name, report_id)
     post_data = {"run_step": run_step, "run_status": run_status}
     print("post_data in update_testreport_step:{0}".format(post_data))
-    res = requests.patch(url=apis, headers=head,
+    res = requests.patch(url=apis, headers=head, proxies=proxies,
                          data=json.dumps(post_data), timeout=5)
     print(res.status_code)
 
@@ -75,7 +75,7 @@ def init_testcaselist(*args):
     post_data = {"test_case_list": test_case_list, "status": 0, "init": True,
                  "gpu_type": gpu_type, "server_os": server_os}
     print("post_data in init_testcaselist:{0}".format(post_data))
-    res = requests.post(url=apis, headers=head,
+    res = requests.post(url=apis, headers=head, proxies=proxies,
                         data=json.dumps(post_data), timeout=5)
     print(res.status_code)
 
@@ -90,7 +90,7 @@ def update_testcase_run_status(*args):
     post_data = {"jenkins_node": jenkins_node, "serverip": serverip,
                  "testcase": testcasename, "update_data": update_data}
     print("post_data in update_testcase_run_status:{0}".format(post_data))
-    res = requests.patch(url=apis, headers=head,
+    res = requests.patch(url=apis, headers=head, proxies=proxies,
                          data=json.dumps(post_data), timeout=5)
     print("status_code:{0}".format(res.status_code))
 
@@ -106,7 +106,7 @@ def update_testcase_report(*args):
     post_data = {"node_name": node_name, "servername": servername, "serverip": serverip,
                  "testcase": testcase_name, "update_data": update_data}
     print("post_data in update_testcase_report:{0}".format(post_data))
-    res = requests.patch(url=apis, headers=head,
+    res = requests.patch(url=apis, headers=head, proxies=proxies,
                          data=json.dumps(post_data), timeout=5)
     print("status_code:{0}".format(res.status_code))
 
@@ -114,9 +114,9 @@ def update_testcase_report(*args):
 @catch_exception
 def update_all_case_abort(*args):
     report_id = args[0]
-    apis = "{0}/testreport/{1}".format(host_name, report_id)
+    apis = "{0}/testreports/{1}".format(host_name, report_id)
     post_data = {"all_abort": True}
-    res = requests.patch(url=apis, headers=head,
+    res = requests.patch(url=apis, headers=head, proxies=proxies,
                          data=json.dumps(post_data), timeout=5)
     print(res.status_code)
 
@@ -124,10 +124,10 @@ def update_all_case_abort(*args):
 @catch_exception
 def update_component(*args):
     report_id, component_name, component_value = args[0], args[1], args[2]
-    apis = "{0}/testreport/{1}".format(host_name, report_id)
+    apis = "{0}/testreports/{1}".format(host_name, report_id)
     post_data = {"components": {component_name: component_value}}
     print("post_data in update_component:{0}".format(post_data))
-    res = requests.patch(url=apis, headers=head,
+    res = requests.patch(url=apis, headers=head, proxies=proxies,
                          data=json.dumps(post_data), timeout=5)
     print("status_code:{0}".format(res.status_code))
 
@@ -138,6 +138,7 @@ if __name__ == "__main__":
     args = tuple(sys.argv[2:])
     requester_ip = socket.gethostbyname(socket.gethostname())
     token = "Token 123412354wsdrfqawerqasdtr123235412341234"
+    proxies = {'http': None, 'https': None}
     host_name = "http://10.21.16.445:8000"
     head = {"Content-Type": "application/json; charset=UTF-8",
             "Authorization": token}
