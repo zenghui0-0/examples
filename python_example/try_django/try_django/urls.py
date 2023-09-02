834a707016564357ee772078479ad697c6454683
zenghui <784515773@qq.com>
2023-09-02 13:00:00 +0800

    Update urls.py

--- python_example/try_django/try_django/urls.py
+++ python_example/try_django/try_django/urls.py
@@ -56,5 +56,6 @@ urlpatterns = [
     url(r"docs/", include_docs_urls(title="My API title")),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('api-auth/', include('rest_framework.urls'))，
 ]
 urlpatterns += router.urls
