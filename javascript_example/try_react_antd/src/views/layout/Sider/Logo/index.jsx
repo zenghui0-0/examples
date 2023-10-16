40017e2040cf8c47359c0c7984a6ba238e9876fe
zenghui <784515773@qq.com>
2023-10-16 11:34:50 +0800

    Update index.jsx

--- javascript_example/try_react_antd/src/views/layout/Sider/Logo/index.jsx
+++ javascript_example/try_react_antd/src/views/layout/Sider/Logo/index.jsx
@@ -5,7 +5,7 @@ const Logo = () => {
   return (
     <div className="sidebar-logo-container">
       <img src={logo} className="sidebar-logo" alt="logo" />
-      <h1 className="sidebar-title">Admin</h1>
+      <h1 className="sidebar-title">hello admin!</h1>
     </div>
   );
 };
